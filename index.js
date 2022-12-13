0a907ece36a1a1835dee20f157846c544d1f4242
eduvilla97 <eduvilla99@gmail.com>
2022-12-13 13:13:18 +0100

    day 2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const { parentPort } = require('worker_threads');
 
 const challenge1 = () => {
 	const calories = fs.readFileSync(path.join(__dirname, 'inputs', 'calories.txt'), 'utf-8').split('\r\n');
@@ -38,4 +39,65 @@ const challenge2 = () => {
 	return top3Elfs.reduce((acc, x) => acc + x, 0)
 }
 
-console.log(challenge2());
+const challenge3 = () => {
+	const gameLogic = {
+		'X': { wins: 'C', draw: 'A', points: 1 },
+		'Y': { wins: 'A', draw: 'B', points: 2 },
+		'Z': { wins: 'B', draw: 'C', points: 3 }
+	}
+	const WON = 6;
+	const DRAW = 3;
+	/*
+	X ------ ROCK --------- A
+	Y ------ PAPER -------- B
+	Z	------ SCISSORS ----- C
+	*/
+	const matches = fs.readFileSync(path.join(__dirname, 'inputs', 'rps.txt'), 'utf-8').split('\n');
+	// const matches = ['C X', 'C X', 'B Y', 'B X'];
+	let plays;
+	let score = 0;
+
+	for (let match of matches) {
+		plays = match.split(' ');
+		if (gameLogic[plays[1]].wins === plays[0]) {
+			score += gameLogic[plays[1]].points + WON;
+		} else
+			if (gameLogic[plays[1]].draw === plays[0]) {
+				score += gameLogic[plays[1]].points + DRAW;
+			} else {
+				score += gameLogic[plays[1]].points;
+			}
+	}
+	return score;
+}
+
+const challenge4 = () => {
+	const gameLogic = {
+		'A': { wins: 2, draws: 1, loses: 3 },
+		'B': { wins: 3, draws: 2, loses: 1 },
+		'C': { wins: 1, draws: 3, loses: 2 }
+	}
+	const WON = 6;
+	const DRAW = 3;
+
+	const matches = fs.readFileSync(path.join(__dirname, 'inputs', 'rps.txt'), 'utf-8').split('\n');
+	// const matches = ['C X', 'C X', 'B Y', 'B X'];
+	let plays;
+	let score = 0;
+
+	for (let match of matches) {
+		plays = match.split(' ');
+		if (plays[1] === 'Z') { // Player wins
+			score += gameLogic[plays[0]].wins + WON;
+		} else
+			if (plays[1] === 'Y') { // DRAW
+				score += gameLogic[plays[0]].draws + DRAW;
+			}
+			else { // Player loses
+				score += gameLogic[plays[0]].loses;
+			}
+	}
+	return score;
+}
+
+console.log(challenge4());
